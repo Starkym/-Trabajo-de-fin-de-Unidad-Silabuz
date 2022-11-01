270c4a84407e5335d22b5ca6214cd2f6438bd65c
JohanPinares <johan_kyurem@hotmail.com>
2022-11-01 16:44:02 -0500

    primeraparte

---
+++ tarea1.py
@@ -0,0 +1,8 @@
+class Libro:
+    def __init__(self,id,titulo,genero,ISBN,editorial,autores) -> None:
+        self.__id=id
+        self.__titulo=titulo
+        self.__genero=genero
+        self.__ISBN=ISBN
+        self.__editorial=editorial
+        self.__autores=autores
