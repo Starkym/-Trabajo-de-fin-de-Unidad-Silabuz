c9726083cd8434d754d0e9684e10ed1113e4a2a0
JohanPinares <johan_kyurem@hotmail.com>
2022-11-01 18:47:08 -0500

    mas datos

--- tarea1.py
+++ tarea1.py
@@ -2,24 +2,31 @@ import os
 import time
 
 #Colores
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
 YELLOW = '\033[33m'
 BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
 RESET = '\033[39m'
-
+print(RED+"------------------PROYECTO 1-----------------")
+print(MAGENTA+"INGRESE LA SOLICITUD REFERENTE A SU NUMERO DE REFERENCIA")
 #Print opciones
 def print_options():
     print(BLUE+"----------------- CRUD LIBROS ---------------"+RESET)
     print(YELLOW+'[1] Leer archivo txt o csv (max 3)')
-    print('[2] Listar libros')
-    print('[3] Agregar Libro')
-    print('[4] Eliminar Libro')
-    print('[5] Buscar por ISBN o titulo')
-    print('[6] Ordenar libros por titulo')
-    print('[7] Buscar por autor, editorial o genero')
-    print('[8] Buscar por numero de autores')
-    print('[9] Editar o actualizar libro')
-    print('[10] Guardar libro en archivo txt o csv')
-    print('[11] Salir'+RESET)
+    print(MAGENTA+'[2] Listar libros')
+    print(RED+'[3] Agregar Libro')
+    print(CYAN+'[4] Eliminar Libro')
+    print(WHITE+'[5] Buscar por ISBN o titulo')
+    print(GREEN+'[6] Ordenar libros por titulo')
+    print(BLUE+'[7] Buscar por autor, editorial o genero')
+    print(YELLOW+'[8] Buscar por numero de autores')
+    print(RED+'[9] Editar o actualizar libro')
+    print(MAGENTA+'[10] Guardar libro en archivo txt o csv')
+    print(CYAN+'[11] Salir'+RESET)
     print("-" * 20)
 
 #Metodo opciones
