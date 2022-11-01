45f7c1b056aeee9cc8acb1f27a16feba96852a27
Starkym <starkymc24@gmail.com>
2022-11-01 17:00:35 -0500

    anotaciones a la clase Libro

--- tarea1.py
+++ tarea1.py
@@ -1,8 +1,11 @@
 class Libro:
-    def __init__(self,id,titulo,genero,ISBN,editorial,autores) -> None:
+    def __init__(self,id:int,titulo:str,genero:str,ISBN: int,editorial:str,autores:str) -> None:
         self.__id=id
         self.__titulo=titulo
         self.__genero=genero
         self.__ISBN=ISBN
         self.__editorial=editorial
         self.__autores=autores
+
+
+
